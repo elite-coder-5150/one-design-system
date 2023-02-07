181831df2d55440c1218bd165bb3d09e18d03914
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-07 12:29:48 -0600

    minified version

---
+++ js/notify-min.js
@@ -0,0 +1,1 @@
+export class Notify{init(){this.cacheDom()}cacheDom(){this.notifyCard=document.querySelector(".notifications-card"),this.bellIcon=document.querySelector(".bell-icon")}initEvents(){}toggleNotification(){}}
