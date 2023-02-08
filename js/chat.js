aec1a7ce517befcf368d3994c229eeb697fdcbe4
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-07 20:56:23 -0600

    refactored add message method

--- js/chat.js
+++ js/chat.js
@@ -18,9 +18,26 @@ export class Chat {
         msg.innerHTML = /*html*/`
             <div class="message-header">
                 <div class="message-header-img">
-                    
+
                 </div>
-            </div>
+                <div class="info">
+                    <div class="user-name">olivia rhey</div>
+                    <div class="message-time">2 hours ago</div>
+                </div> <!-- end of .info -->
+
+                <div class="actions">
+                    <button>
+                        <div class="icon">
+                            <div class="trash-icon"></div>
+                    </button>
+                </div> <!-- end of .actions -->
+                
+                <div class="message-body">
+                    <p class="text">>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod</p>
+                </div>
+            </div> <!-- end of .message-header -->
         `;
+
+        this.messageCard.appendChild(msg);
     }
 }
