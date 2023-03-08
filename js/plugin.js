2aeb9ee7b54604b0005a845f91b9363a254c8fe6
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-03-07 20:40:42 -0600

    defined the drop down menu class

--- js/plugin.js
+++ js/plugin.js
@@ -36,3 +36,54 @@ $.fn.modal = (options) => {
         }
     });
 };
+
+class DropDownMenu {
+    constructor(elem, option) {
+        this.elem = $(elem);
+        this.options = option;
+    }
+
+    init() {
+        this.cacheDom();
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.button.on('click', () => {
+            this.rotateIcon();
+            this.toggle();
+        });
+    }
+    cacheDom() {
+        this.menu = this.elem.find('.dropdown-menu');
+        this.button = this.elem.find('.dropdown-button');
+        this.icon = document.querySelector('.chevron-down');
+    }
+    toggle() {
+        if (!(this.elem.hasClass('open'))) {
+            this.open();
+        } else {
+            this.close();
+        }
+    }
+
+    rotateIcon() {
+        this.icon.style.transform = 'rotate(180deg)';
+    }
+
+    open() {
+
+    }
+
+    close() {
+        this.elem.removeClass('open');
+    }
+}
+
+$.fn.dropdown = (options) => {
+    return this.each(() => {
+        if ($.data(this, 'plugin_dropdown')) {
+            $.data(this, 'plugin_dropdown', new DropDownMenu(this, options));
+        }
+    });
+};
